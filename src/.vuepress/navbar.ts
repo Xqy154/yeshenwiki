3bed3bc21e2408a3400927ab5ec41bc48ff5f63e
xqy154 <1549386753@qq.com~>
2025-02-02 22:38:43 +0800

    目录更改

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -4,44 +4,37 @@ export default navbar([
   "/",
   "/demo/",
   {
-    text: "博文",
+    text: "目录",
     icon: "pen-to-square",
     prefix: "/posts/",
     children: [
       {
         text: "苹果",
         icon: "pen-to-square",
-        prefix: "apple/",
+        prefix: "/教程/",
         children: [
-          { text: "苹果1", icon: "pen-to-square", link: "1" },
+          { text: "新手教程", icon: "pen-to-square", link: "1" },
           { text: "苹果2", icon: "pen-to-square", link: "2" },
           "3",
           "4",
-        ],
-      },
-      {
-        text: "香蕉",
-        icon: "pen-to-square",
-        prefix: "banana/",
-        children: [
           {
             text: "香蕉 1",
             icon: "pen-to-square",
-            link: "1",
+            link: "5",
           },
           {
             text: "香蕉 2",
             icon: "pen-to-square",
-            link: "2",
+            link: "6",
           },
           "3",
           "4",
         ],
       },
-      { text: "樱桃", icon: "pen-to-square", link: "cherry" },
-      { text: "火龙果", icon: "pen-to-square", link: "dragonfruit" },
-      "tomato",
-      "strawberry",
+
+      // { text: "樱桃", icon: "pen-to-square", link: "cherry" },
+      // { text: "火龙果", icon: "pen-to-square", link: "dragonfruit" },
+
     ],
   },
   // {
